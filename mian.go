d6cfd67f112703f455c5d17312634afcc24b9361
Pramod Sannagowdru <pramodsannagowdru@gmail.com>
2025-05-23 14:35:00 +0530

    routers added into the main

--- mian.go
+++ mian.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Books struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) SetUpRoutes(api *gin.RouterGroup) {
+	api.Group("/api")
+	api.POST("?addbooks")
+	api.GET("/getbooks")
+	api.GET("/getbook/:id")
+	api.DELETE("/deletebook")
+}
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	router := gin.Default()
 	fmt.Println("Hello Postgres")
+	router.Run(":3010")
 }
